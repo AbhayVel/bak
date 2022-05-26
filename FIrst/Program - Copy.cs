0b45cec2dadee5fa746e519502653c85ddd9fb1c
Baktygul <baktygul.aliev@gmail.com>
2022-05-26 08:49:17 -0500

    done first exercise

---
+++ FIrst/Program - Copy.cs
@@ -0,0 +1,160 @@
+﻿//using System;
+
+//namespace FIrst
+//{
+
+
+//   //cutom data type 
+//    delegate bool bak(string str); //Method signature->Datatype 
+
+//    //Hold method => with same siganature 
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Datatype variableName=Hold intgeger value;
+//            int a = 0;
+
+//            //Datattype variable =hold string value 
+//            string na = "aa";
+//            //w/s 
+//            //String 
+//            //number 
+//            //boolean 
+//            //array/collection  
+//            //dateTIme 
+
+//            string str = "Bak al";
+//          var d=  str.StartsWith("s");
+
+//            var employees =new string[] { "Bak", "John", "jhon", "Abhay", "Mac", "Kally", "Bal" };
+
+//            //Annonomious Method
+//            //var resultA = GetWhere(employees, (x)=>x.StartsWith("A"));
+//            //var resultB = GetWhere(employees, (x) => x.StartsWith("B"));
+
+//            //Print(resultA);
+//            //Print(resultB);
+
+//            var resultk= GetWhere(employees, Condition );
+//            var resulty  = GetWhere(employees, (x) => x.StartsWith("y"));
+
+//            Print(resultk);
+//           // Print(resulty);
+
+
+//            Console.WriteLine("Hello World!");
+//        }
+
+
+//        static bool Condition(string str)
+//        {
+//            return str.StartsWith("A");
+//        }
+
+//        public static string[] GetWhere(string[] employees, bak funct)
+//        {
+//            string[] result = new string[employees.Length];
+//            int j = 0;
+//            for (int i = 0; i < employees.Length; i = i + 1)
+//            {
+//                if (funct(employees[i]))
+//                {
+//                    result[j] = employees[i];
+//                    j = j + 1;
+//                }
+//            }
+//            return result;
+//        }
+         
+//            public static string[] XXX(string[] employees, string val,string condition)
+//        {
+//            string[] result = new string[employees.Length];
+//            int j = 0;
+//            for (int i = 0; i < employees.Length; i = i + 1)
+//            {
+//                if(condition.Equals("Contains", StringComparison.OrdinalIgnoreCase))
+//                {
+//                    if (employees[i].Contains(val))
+//                    {
+//                        result[j] = employees[i];
+//                        j = j + 1;
+//                    }
+//                } else if (condition.Equals("StartsWith", StringComparison.OrdinalIgnoreCase))
+//                {
+//                    if (employees[i].StartsWith(val))
+//                    {
+//                        result[j] = employees[i];
+//                        j = j + 1;
+//                    }
+//                }
+//                else if (condition.Equals("EndsWith", StringComparison.OrdinalIgnoreCase))
+//                {
+//                    if (employees[i].EndsWith(val))
+//                    {
+//                        result[j] = employees[i];
+//                        j = j + 1;
+//                    }
+//                }
+//            }
+//            return result;
+//        }
+
+
+
+
+//        public static string[] GetContains(string[] employees, string val)
+//        {
+//            string[] result = new string[employees.Length];
+//            int j = 0;
+//            for (int i = 0; i < employees.Length; i = i + 1)
+//            {
+//                if (employees[i].Contains(val))
+//                {
+//                    result[j] = employees[i];
+//                    j = j + 1;
+//                }
+//            }
+//            return result;
+//        }
+
+//        public static string[] GetEndWith(string[] employees, string val)
+//        {
+//            string[] result = new string[employees.Length];
+//            int j = 0;
+//            for (int i = 0; i < employees.Length; i = i + 1)
+//            {
+//                if (employees[i].EndsWith(val))
+//                {
+//                    result[j] = employees[i];
+//                    j = j + 1;
+//                }
+//            }
+//            return result;
+//        }
+
+//        public static string[] GetStartWith(string[] employees, string val)
+//        {
+//            string[] result= new string[employees.Length];
+//            int j = 0;
+//            for (int i = 0; i < employees.Length; i = i + 1)
+//            {
+//                if (employees[i].StartsWith(val))
+//                {
+//                    result[j] = employees[i];
+//                    j = j + 1;
+//                }
+//            }
+//            return result;
+//        }
+
+//        public static void Print(string[] employees)
+//        {
+//            for (int i = 0; i < employees.Length; i = i + 1)
+//            {
+//                Console.WriteLine(employees[i]);
+//            }
+//        }
+
+//    }
+//}
--- FIrst/Program.cs
+++ FIrst/Program.cs
@@ -3,149 +3,209 @@
 namespace FIrst
 {
 
-    delegate bool Funct(string str);
+
+    //cutom data type 
+   delegate bool FunWhere(int str); //Method signature->Datatype  //Deleget 
+
+    //Hold method => with same siganature 
     internal class Program
     {
         static void Main(string[] args)
         {
-            //w/s 
-            //String 
-            //number 
-            //boolean 
-            //array/collection  
-            //dateTIme 
+            int[] employee = new int[] { 1, 34, 56, 12, 78, 89, 29, 77, 66 };
 
-            string str = "Bak al";
-          var d=  str.StartsWith("s");
+            //int[] answer = SortNumbers(employee, 20);
+            //int[] answer = CombinedFunc(employee, 20, "bottom");
+            int[] answer = SortNumbers(employee, (x) => x > 30);  //call back method/annonomious method
+                                                                  //int[] answer2 = SortNumbers2(employee, 80);
+                                                                  // int[] answer2 = CombinedFunc(employee, 80, "top");
+            int[] answer2 = SortNumbers(employee, (x) => x < 80);
+            //int[] answer3 = SortNumbers3(employee, 2);
+            int[] answer3 = CombinedFunc(employee, 2, "divisor");
 
-            var employees =new string[] { "Bak", "John", "jhon", "Abhay", "Mac", "Kally", "Bal" };
-
-            //Annonomious Method
-            var resultA = GetWhere(employees, (x)=>x.StartsWith("A"));
-            var resultB = GetWhere(employees, (x) => x.StartsWith("B"));
-
-            Print(resultA);
-            Print(resultB);
-
-            var resultk= GetWhere(employees, (x) => x.Contains("k"));
-            var resulty  = GetWhere(employees, (x) => x.EndsWith("y"));
-
-            Print(resultk);
-            Print(resulty);
+            //Print(answer);
+            //Print(answer2);
+            Console.WriteLine("answer 1:");
+            Print(answer);
+            Console.WriteLine("answer 2:");
+            Print(answer2);
+            Console.WriteLine("answer 3:");
+            Print(answer3);
 
 
-            Console.WriteLine("Hello World!");
         }
 
-
-        static bool Condition(string str)
+        public static int[] SortNumbers(int[] array, FunWhere func)
         {
-            return str.EndsWith("a");
-        }
+            int[] result = new int[array.Length];
+            int counter = 0;
 
-        public static string[] GetWhere(string[] employees, Funct funct)
-        {
-            string[] result = new string[employees.Length];
-            int j = 0;
-            for (int i = 0; i < employees.Length; i = i + 1)
+            for (int i = 0; i < array.Length; i++)
             {
-                if (funct(employees[i]))
+                if (func(array[i]))
                 {
-                    result[j] = employees[i];
-                    j = j + 1;
+                    result[counter] = array[i];
+                    counter++;
                 }
             }
-            return result;
-        }
-         
-            public static string[] XXX(string[] employees, string val,string condition)
-        {
-            string[] result = new string[employees.Length];
-            int j = 0;
-            for (int i = 0; i < employees.Length; i = i + 1)
+            int[] result2 = new int[counter];
+            int counter2 = 0;
+            for (int i = 0; i < result.Length; i++)
             {
-                if(condition.Equals("Contains", StringComparison.OrdinalIgnoreCase))
+                if (result[i] != 0)
                 {
-                    if (employees[i].Contains(val))
+                    result2[counter2] = result[i];
+                    counter2++;
+                }
+            }
+            return result2;
+        }
+
+        public static int[] SortNumbers2(int[] array, int upperLimit)
+        {
+            int[] result = new int[array.Length];
+            int counter = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= upperLimit)
+                {
+                    result[counter] = array[i];
+                    counter++;
+                }
+            }
+            int[] result2 = new int[counter];
+            int counter2 = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] != 0)
+                {
+                    result2[counter2] = result[i];
+                    counter2++;
+                }
+            }
+            return result2;
+        }
+
+        public static int[] SortNumbers3(int[] array, int divisor)
+        {
+            int[] result = new int[array.Length];
+            int counter = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % divisor == 0)
+                {
+                    result[counter] = array[i];
+                    counter++;
+                }
+            }
+            int[] result2 = new int[counter];
+            int counter2 = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] != 0)
+                {
+                    result2[counter2] = result[i];
+                    counter2++;
+                }
+            }
+            return result2;
+        }
+
+        public static int[] CombinedFunc(int[] array, int number, string condition)
+        {
+            int[] result = new int[array.Length];
+            int counter = 0;
+
+            if (condition.Equals("top", StringComparison.OrdinalIgnoreCase))
+            {
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] <= number)
                     {
-                        result[j] = employees[i];
-                        j = j + 1;
+                        result[counter] = array[i];
+                        counter++;
                     }
-                } else if (condition.Equals("StartsWith", StringComparison.OrdinalIgnoreCase))
+                }
+
+                int[] result2 = new int[counter];
+                int counter2 = 0;
+
+                for (int i = 0; i < result.Length; i++)
                 {
-                    if (employees[i].StartsWith(val))
+                    if (result[i] != 0)
                     {
-                        result[j] = employees[i];
-                        j = j + 1;
+                        result2[counter2] = result[i];
+                        counter2++;
                     }
                 }
-                else if (condition.Equals("EndsWith", StringComparison.OrdinalIgnoreCase))
+                return result2;
+            }
+
+            else if (condition.Equals("bottom", StringComparison.OrdinalIgnoreCase))
+            {
+
+                for (int i = 0; i < array.Length; i++)
                 {
-                    if (employees[i].EndsWith(val))
+                    if (array[i] >= number)
                     {
-                        result[j] = employees[i];
-                        j = j + 1;
+                        result[counter] = array[i];
+                        counter++;
                     }
                 }
-            }
-            return result;
-        }
 
+                int[] result2 = new int[counter];
+                int counter2 = 0;
 
-
-
-        public static string[] GetContains(string[] employees, string val)
-        {
-            string[] result = new string[employees.Length];
-            int j = 0;
-            for (int i = 0; i < employees.Length; i = i + 1)
-            {
-                if (employees[i].Contains(val))
+                for (int i = 0; i < result.Length; i++)
                 {
-                    result[j] = employees[i];
-                    j = j + 1;
+                    if (result[i] != 0)
+                    {
+                        result2[counter2] = result[i];
+                        counter2++;
+                    }
                 }
+                return result2;
             }
-            return result;
-        }
-
-        public static string[] GetEndWith(string[] employees, string val)
-        {
-            string[] result = new string[employees.Length];
-            int j = 0;
-            for (int i = 0; i < employees.Length; i = i + 1)
+            else if (condition.Equals("divisor", StringComparison.OrdinalIgnoreCase))
             {
-                if (employees[i].EndsWith(val))
-                {
-                    result[j] = employees[i];
-                    j = j + 1;
-                }
-            }
-            return result;
-        }
 
-        public static string[] GetStartWith(string[] employees, string val)
-        {
-            string[] result= new string[employees.Length];
-            int j = 0;
-            for (int i = 0; i < employees.Length; i = i + 1)
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] % number == 0)
+                    {
+                        result[counter] = array[i];
+                        counter++;
+                    }
+                }
+
+                int[] result2 = new int[counter];
+                int counter2 = 0;
+
+                for (int i = 0; i < result.Length; i++)
+                {
+                    if (result[i] != 0)
+                    {
+                        result2[counter2] = result[i];
+                        counter2++;
+                    }
+                }
+                return result2;
+            } else
             {
-                if (employees[i].StartsWith(val))
-                {
-                    result[j] = employees[i];
-                    j = j + 1;
-                }
+                return null;
             }
-            return result;
+
         }
 
-        public static void Print(string[] employees)
+        private static void Print(int[] employees)
         {
             for (int i = 0; i < employees.Length; i = i + 1)
             {
                 Console.WriteLine(employees[i]);
             }
         }
-
     }
 }
